95cb9f619119b771f0f101598c7656201432d59d
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-04-13 14:32:36 -0300

    feat: add auth module to global routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,15 @@
+import express, { type Express, type Request, type Response } from "express";
+import cors from "cors";
+import authRoutes from "../modules/auth/routes/auth.routes";
+
+const setupRoutes = (app: Express) => {
+  app.use(express.json());
+  app.use(cors());
+  app.get("/server-test", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Server is running!" });
+  });
+
+  app.use(authRoutes);
+};
+
+export { setupRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { app } from "./app";
+import { setupRoutes } from "./routes/routes";
+
+setupRoutes(app);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
