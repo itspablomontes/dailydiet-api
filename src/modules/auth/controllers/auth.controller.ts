d58c9a81594d802fdf1768942bcc8ab2513178d3
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-04-14 22:06:06 -0300

    feat: add login controller method

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import { AuthService } from "../services/auth.service";
-import { registerSchema } from "../schemas/auth.schema";
+import { loginSchema, registerSchema } from "../schemas/auth.schema";
 import { ZodError } from "zod";
 import { AuthRepository } from "../repositories/auth.repository";
 
@@ -18,10 +18,39 @@ class AuthController {
       res.status(201).json(result);
     } catch (error) {
       if (error instanceof ZodError) {
-        res.status(400).json({ errors: error.errors });
+        res.status(400).json({
+          error: "Invalid Input",
+          details: error.errors.map((e) => e.message),
+        });
       } else if (
         error instanceof Error &&
         error.message === "Email already in use"
+      ) {
+        res.status(409).json({ error: error.message });
+      } else {
+        next(error);
+      }
+    }
+  };
+
+  login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const input = loginSchema.parse(req.body);
+      const result = await this.authService.login(input);
+      res.status(200).json(result);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res.status(400).json({
+          error: "Invalid Input",
+          details: error.errors.map((e) => e.message),
+        });
+      } else if (
+        error instanceof Error &&
+        error.message === "Account not found"
       ) {
         res.status(409).json({ error: error.message });
       } else {
