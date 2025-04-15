9f0c916b66e57da51227e826159c12541f1387aa
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-04-14 22:01:02 -0300

    feat: add login service method

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -6,6 +6,12 @@ import type { IAuthRepository } from "../interfaces/auth.repository.interface";
 export class AuthService {
   constructor(private authRepository: IAuthRepository) {}
 
+  private generateToken(userId: number): string {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET!, {
+      expiresIn: "1d",
+    });
+  }
+
   async register(data: {
     name: string;
     email: string;
@@ -24,13 +30,43 @@ export class AuthService {
       password: hashedPassword,
     });
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, {
-      expiresIn: "1d",
-    });
+    const token = this.generateToken(user.id);
 
     return {
       token,
-      user,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    };
+  }
+
+  async login(data: {
+    email: string;
+    password: string;
+  }): Promise<{ token: string; user: Partial<User> }> {
+    const existingUser = await this.authRepository.findByEmail(data.email);
+
+    if (!existingUser) {
+      throw new Error("This account doesn't exist");
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      data.password,
+      existingUser.password,
+    );
+    if (!isPasswordValid) {
+      throw new Error("Invalid Password");
+    }
+
+    const token = this.generateToken(existingUser.id);
+    return {
+      token,
+      user: {
+        email: existingUser.email,
+        name: existingUser.name,
+      },
     };
   }
 }
