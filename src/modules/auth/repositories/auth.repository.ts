bceb2c3ae3c62159d7931cae79af53da388812ce
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-04-15 22:49:02 -0300

    feat: add session handling repository queries

--- src/modules/auth/repositories/auth.repository.ts
+++ src/modules/auth/repositories/auth.repository.ts
@@ -1,5 +1,5 @@
 import prisma from "../../../config/database";
-import type { User } from "@prisma/client";
+import type { Session, User } from "@prisma/client";
 import type { IAuthRepository } from "../interfaces/auth.repository.interface";
 
 export class AuthRepository implements IAuthRepository {
@@ -8,7 +8,41 @@ export class AuthRepository implements IAuthRepository {
   ): Promise<User> {
     return prisma.user.create({ data });
   }
-  async findByEmail(email: string): Promise<User | null> {
+
+  async findUserByEmail(email: string): Promise<User | null> {
     return prisma.user.findUnique({ where: { email } });
+  }
+  async findUserbyId(id: number): Promise<User | null> {
+    return prisma.user.findUnique({ where: { id } });
+  }
+
+  async createSession(
+    userId: number,
+    refreshToken: string,
+    expiresAt: Date,
+  ): Promise<Session> {
+    return prisma.session.create({
+      data: { userId, refreshToken, expires_at: expiresAt },
+    });
+  }
+
+  async findSessionByRefreshToken(
+    refreshToken: string,
+  ): Promise<Session | null> {
+    return prisma.session.findUnique({
+      where: {
+        refreshToken,
+      },
+    });
+  }
+
+  async deleteSession(refreshToken: string): Promise<void> {
+    await prisma.session.delete({ where: { refreshToken } });
+  }
+
+  async deleteExpiredSessions(): Promise<void> {
+    await prisma.session.deleteMany({
+      where: { expires_at: { lte: new Date() } },
+    });
   }
 }
