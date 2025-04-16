3ff2daf0072919eaa42d6530cacd3b0846ba9777
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-04-15 22:48:36 -0300

    feat: add session handling interfaces

--- src/modules/auth/interfaces/auth.repository.interface.ts
+++ src/modules/auth/interfaces/auth.repository.interface.ts
@@ -1,8 +1,17 @@
-import type { User } from "@prisma/client";
+import type { Session, User } from "@prisma/client";
 
 export interface IAuthRepository {
   createUser(
     data: Omit<User, "id" | "created_at" | "updated_at" | "isVerified">,
   ): Promise<User>;
-  findByEmail(email: string): Promise<User | null>;
+  findUserByEmail(email: string): Promise<User | null>;
+  findUserbyId(id: number): Promise<User | null>;
+  createSession(
+    userId: number,
+    refreshToken: string,
+    expiresAt: Date,
+  ): Promise<Session>;
+  findSessionByRefreshToken(refreshToken: string): Promise<Session | null>;
+  deleteSession(refreshToken: string): Promise<void>;
+  deleteExpiredSessions(): Promise<void>;
 }
