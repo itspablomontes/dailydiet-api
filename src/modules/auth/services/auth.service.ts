0dec4c993548c7ea615b46298c5c1ee51b821a86
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-04-15 22:49:20 -0300

    feat: add session handling service methods

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -2,13 +2,14 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import type { User } from "@prisma/client";
 import type { IAuthRepository } from "../interfaces/auth.repository.interface";
+import { randomUUID } from "node:crypto";
 
 export class AuthService {
   constructor(private authRepository: IAuthRepository) {}
 
   private generateToken(userId: number): string {
     return jwt.sign({ id: userId }, process.env.JWT_SECRET!, {
-      expiresIn: "1d",
+      expiresIn: "1h",
     });
   }
 
@@ -17,7 +18,7 @@ export class AuthService {
     email: string;
     password: string;
   }): Promise<{ token: string; user: Partial<User> }> {
-    const existingUser = await this.authRepository.findByEmail(data.email);
+    const existingUser = await this.authRepository.findUserByEmail(data.email);
     if (existingUser) {
       throw new Error("Email already in use");
     }
@@ -45,8 +46,8 @@ export class AuthService {
   async login(data: {
     email: string;
     password: string;
-  }): Promise<{ token: string; user: Partial<User> }> {
-    const existingUser = await this.authRepository.findByEmail(data.email);
+  }): Promise<{ token: string; refreshToken: string; user: Partial<User> }> {
+    const existingUser = await this.authRepository.findUserByEmail(data.email);
 
     if (!existingUser) {
       throw new Error("This account doesn't exist");
@@ -61,12 +62,60 @@ export class AuthService {
     }
 
     const token = this.generateToken(existingUser.id);
+
+    const refreshToken = randomUUID();
+    const expiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // 30 days
+    await this.authRepository.createSession(
+      existingUser.id,
+      refreshToken,
+      expiresAt,
+    );
+
     return {
       token,
+      refreshToken,
       user: {
         email: existingUser.email,
         name: existingUser.name,
       },
     };
+  }
+
+  async refresh(refreshToken: string) {
+    const session =
+      await this.authRepository.findSessionByRefreshToken(refreshToken);
+
+    if (!session || session.expires_at < new Date()) {
+      throw new Error("Invalid or expired session");
+    }
+
+    const user = await this.authRepository.findUserbyId(session.userId);
+
+    if (!user) {
+      throw new Error("User not found!");
+    }
+
+    const token = this.generateToken(user.id);
+
+    return {
+      token,
+      user: {
+        id: user?.id,
+      },
+    };
+  }
+
+  async logout(refreshToken: string, userId: number) {
+    const session =
+      await this.authRepository.findSessionByRefreshToken(refreshToken);
+    if (!session || session.userId !== userId) {
+      throw new Error("Invalid Session");
+    }
+
+    await this.authRepository.deleteSession(refreshToken);
+  }
+
+  async cleanupSessions(): Promise<void> {
+    await this.authRepository.deleteExpiredSessions();
   }
 }
